61419ead084dd16db6ac4b4f56b7610172923543
Yevhen Shymotyuk <fly1ngdream@pm.me>
2020-05-12 22:43:05 +0300

    Add SeatLive.Selected live view

--- lib/cinema_web/live/seat_live/seats_component.ex
+++ lib/cinema_web/live/seat_live/seats_component.ex
@@ -1,18 +1,30 @@
 defmodule CinemaWeb.SeatLive.SeatsComponent do
   use CinemaWeb, :live_component
 
+  @seats_in_row 5
+
   def update(assigns, socket) do
     seats =
       assigns.seats
       |> Enum.sort_by(& &1.number)
-      |> Enum.chunk_every(5)
+      |> Enum.chunk_every(@seats_in_row)
+
+    row = fn n ->
+      if rem(n, @seats_in_row) == 0 do
+        div(n, @seats_in_row)
+      else
+        div(n, @seats_in_row) + 1
+      end
+    end
 
     {
       :ok,
       socket
       |> assign(assigns)
+      |> assign(hall_id: assigns.id)
       |> assign(seats: seats)
       |> assign(selected_seats: Enum.reverse(assigns.selected_seats))
+      |> assign(row: row)
     }
   end
 end
---
+++ lib/cinema_web/live/seat_live/selected.ex
@@ -0,0 +1,55 @@
+defmodule CinemaWeb.SeatLive.Selected do
+  use CinemaWeb, :live_view
+
+  alias Cinema.{Halls, Seats}
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  @impl true
+  def handle_params(
+    %{"hall_id" => hall_id, "selected_seat_ids" => selected_seat_ids},
+    _,
+    socket
+  ) do
+    hall = Halls.get_hall!(hall_id)
+    IO.inspect(selected_seat_ids)
+    selected_seats_data =
+      selected_seat_ids
+      |> String.split(",")
+      |> Enum.map(
+        fn x ->
+          [id, row] = String.split(x, "|")
+
+          %{seat: Seats.get_seat!(id), row: row}
+        end
+      )
+
+    {
+      :noreply,
+      socket
+      |> assign(:page_title, page_title(socket.assigns.live_action))
+      |> assign(:hall, hall)
+      |> assign(:selected_seats_data, selected_seats_data)
+    }
+  end
+
+  @impl true
+  def handle_event("buy-tickets", _, socket) do
+    selected_seats = Enum.map(socket.assigns.selected_seats_data, & &1.seat)
+
+    Enum.each(selected_seats, & Seats.create_ticket(&1))
+
+    {
+      :noreply,
+      socket
+      |> assign(selected_seats: [])
+      |> push_redirect(to: Routes.seat_index_path(socket, :index, socket.assigns.hall.id))
+      |> put_flash(:info, "Tickets were successfully bought")
+    }
+  end
+
+  defp page_title(:selected), do: "Selected seats"
+end
